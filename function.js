dcfbe313f79aa1d2de05ea2020bef71dac8a9080
Victor Tran <vicfortran@gmail.com>
2017-03-07 19:59:08 -0500

    Center cuisineType labels and lowered opacity of non-selected icons
    Center cuisineType labels and lowered opacity of non-selected icons

--- function.js
+++ function.js
@@ -18,14 +18,15 @@ function create_graph(data, ing_index, focus_group, axis, scale, groupings_index
 		//creates axises and background grey rectangle for each cuisine type, axises are currently left aligned
 		//axises need to be updated to start in the center if we're still doing that
 		var x_coord = 22 + (i*(10 + scale(90)));
-		svg2.append("g").attr("transform", "translate(" + x_coord + ", 40)").call(axis);
+		svg2.append("g").attr("transform", "translate(" + x_coord + ", 42)").call(axis);
 		svg2.append("text")
-		.attr("x", x_coord)
+		.attr("x", x_coord + (scale(90) +3)/2)
 		.attr("y", 15)
+		.attr("text-anchor", "middle")
 		.text(cuisine_types[i]);
 		svg2.append("rect")
 		.attr("x", x_coord)
-		.attr("y", 45)
+		.attr("y", 47)
 		.attr("width", scale(90) +3)
 		.attr("height", ing_index.length * 50)
 		.attr("class", "background");
@@ -36,21 +37,23 @@ function create_graph(data, ing_index, focus_group, axis, scale, groupings_index
 			var width = scale(sortedObj[ingredient])
 			svg2.append("rect")
 			.attr("x", x_coord)
-			.attr("y", 47+(j*50))
+			.attr("y", 49+(j*50))
 			.attr("width", width)
 			.attr("height", 45)
 			.attr("id", ingredient+groupings_index+i+"rect")
-			if (focus_group[ingredient] == ingredient){
-				d3.select("#"+ingredient+groupings_index+i+"rect").attr("class", ingredient);
-			}
 			d3.select("#icons"+groupings_index).append("svg:image")
 			.attr("xlink:href", "images/"+ingredient+".svg")
 			.attr("height", 35)
-			.attr("id", ingredient + groupings_index+ i);
+			.attr("id", ingredient + groupings_index+ i)
+			.style("opacity", .35);
 			x_trans = x_coord + width + 7;
 			var transformation = "translate("+ x_trans + ', '+ (53+(j*50)) +')';
 			d3.select("#"+ingredient+groupings_index+i).attr("transform", transformation)
 			j++;
+			if (focus_group[ingredient] == ingredient){
+				d3.select("#"+ingredient+groupings_index+i+"rect").attr("class", ingredient);
+				d3.select("#"+ingredient+groupings_index+i).style("opacity", 1);
+			}
 		}
 	});
 }
