991dd983f403e904ccb0c69fa3e38fc8b2ffa5c8
bullardla <bullardla@gmail.com>
2017-03-07 21:20:46 -0500

    Update function.js

--- function.js
+++ function.js
@@ -3,10 +3,23 @@ function create_graph(data, ing_index, focus_group, axis, scale, groupings_index
 	var svg = d3.select(graph_number);
 	var svg_height = 55+ ing_index.length * 50;
 	svg.attr("height", svg_height);
-	var svg_width = 22 + cuisine_types.length*(10 + scale(90));
+	var svg_width = 22 + cuisine_types.length*(10 + scale(0.9));
  	d3.select(graph_number).attr("width", svg_width);
  	var svg2 = d3.select("#barplot"+groupings_index);
  	console.log(svg2);
+	for(var i = 0; i < 19; i++){
+	    if (i + 1 < 10){
+			svg2.append("text")
+			.attr("x", 8)
+			.attr("y", 68 + (i * 50))
+			.text(i + 1);
+		} else {
+			svg2.append("text")
+			.attr("x", 3)
+			.attr("y", 68 + (i * 50))
+			.text(i + 1);
+		}
+	}
 	data.forEach(function(d, i){
 		var obj = {};
 		ing_index.forEach(function(ingredient){
@@ -17,43 +30,41 @@ function create_graph(data, ing_index, focus_group, axis, scale, groupings_index
 		//console.log(sortedObj[Object.keys(sortedObj)[0]]);
 		//creates axises and background grey rectangle for each cuisine type, axises are currently left aligned
 		//axises need to be updated to start in the center if we're still doing that
-		var x_coord = 22 + (i*(10 + scale(90)));
-		svg2.append("g").attr("transform", "translate(" + x_coord + ", 42)").call(axis);
+		var x_coord = 22 + (i*(20 + scale(0.9)));
+		svg2.append("g").style("font-size", "8px").attr("transform", "translate(" + x_coord + ", 40)").call(axis);
 		svg2.append("text")
-		.attr("x", x_coord + (scale(90) +3)/2)
+		.attr("x", x_coord)
 		.attr("y", 15)
-		.attr("text-anchor", "middle")
 		.text(cuisine_types[i]);
 		svg2.append("rect")
 		.attr("x", x_coord)
-		.attr("y", 47)
-		.attr("width", scale(90) +3)
+		.attr("y", 45)
+		.attr("width", scale(.9) + 3)
 		.attr("height", ing_index.length * 50)
+		.style("fill-opacity", .15)
 		.attr("class", "background");
 		var counts = Object.values(sortedObj);
 		//console.log(counts);
 		var j = 0;
 		for (var ingredient in sortedObj) {
-			var width = scale(sortedObj[ingredient])
+			var width = scale(sortedObj[ingredient] / 100)
 			svg2.append("rect")
 			.attr("x", x_coord)
-			.attr("y", 49+(j*50))
+			.attr("y", 47+(j*50))
 			.attr("width", width)
 			.attr("height", 45)
 			.attr("id", ingredient+groupings_index+i+"rect")
+			if (focus_group[ingredient] == ingredient){
+				d3.select("#"+ingredient+groupings_index+i+"rect").attr("class", ingredient);
+			}
 			d3.select("#icons"+groupings_index).append("svg:image")
 			.attr("xlink:href", "images/"+ingredient+".svg")
 			.attr("height", 35)
-			.attr("id", ingredient + groupings_index+ i)
-			.style("opacity", .35);
+			.attr("id", ingredient + groupings_index+ i);
 			x_trans = x_coord + width + 7;
 			var transformation = "translate("+ x_trans + ', '+ (53+(j*50)) +')';
 			d3.select("#"+ingredient+groupings_index+i).attr("transform", transformation)
 			j++;
-			if (focus_group[ingredient] == ingredient){
-				d3.select("#"+ingredient+groupings_index+i+"rect").attr("class", ingredient);
-				d3.select("#"+ingredient+groupings_index+i).style("opacity", 1);
-			}
 		}
 	});
 }
